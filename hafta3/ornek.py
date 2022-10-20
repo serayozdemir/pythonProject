2472bbcb1f72657305f7cec8ca8e0178209a63e2
seray <melike_ozdemir_@outlook.com>
2022-10-20 13:59:31 +0300

    our exercise

--- hafta3/ornek.py
+++ hafta3/ornek.py
@@ -1,2 +1,17 @@
 '''soru:  kendisine gönderilen sayılardan sadece palindrom olanları toplayan diğerlerini de bu toplamdan çıkaran
-ve geri gönderen python kodunu yazınız'''
+ve geri gönderen python kodunu yazınız palindrom: sağdan ve soldan aynı gözüken'''
+def palindrom(*dram):
+    toplam= fark=0
+    for sayi in dram:
+        if str(sayi) == str(sayi)[::-1]:
+            toplam += sayi
+        else:
+            toplam -=sayi
+    return toplam - fark
+
+palindrom(10,101,55,40,9009)
+
+
+
+
+
