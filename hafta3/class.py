0da7c16463b1e6c931713f6804e2800e27863214
seray <melike_ozdemir_@outlook.com>
2022-10-20 14:05:42 +0300

    our new exercise

---
+++ hafta3/class.py
@@ -0,0 +1,7 @@
+class sinif:
+    pass
+
+
+nesne=sinif()
+
+print(type(nesne))
---
+++ hafta3/method.py
@@ -0,0 +1,12 @@
+class sinif:
+    a=5
+    b="asd"
+    c=[1,3,5]
+
+    def yazdir(self):
+        d=100
+        print(d, self.a)
+
+
+nesne=sinif()
+nesne.yazdir()
